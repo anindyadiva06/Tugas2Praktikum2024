8ba71d9bd1505c35225c72444e9d5ae851f183ea
anindyadiva06 <134478029+anindyadiva06@users.noreply.github.com>
2024-09-20 07:14:49 +0700

    Update form_data.dart

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -1,6 +1,5 @@
 import '/ui/tampil_data.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class FormData extends StatefulWidget {
   const FormData({Key? key}) : super(key: key);
@@ -13,11 +12,19 @@ class FormDataState extends State<FormData> {
   final _namaController = TextEditingController();
   final _nimController = TextEditingController();
   final _tahunController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Input Data"),
+        title: const Text(
+          "Input Data",
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 24, 
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 23, 178, 189), 
       ),
       body: Container(
         margin: const EdgeInsets.all(10),
@@ -26,7 +33,7 @@ class FormDataState extends State<FormData> {
             _textboxNama(),
             _textboxNIM(),
             _textboxTahun(),
-            _tombolSimpan()
+            _tombolSimpan() 
           ],
         ),
       ),
@@ -56,14 +63,19 @@ class FormDataState extends State<FormData> {
 
   _tombolSimpan() {
     return ElevatedButton(
-        onPressed: () {
-          String nama = _namaController.text;
-          String nim = _nimController.text;
-          int tahun = int.parse(_tahunController.text);
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) =>
-                  TampilData(nama: nama, nim: nim, tahun: tahun)));
-        },
-        child: const Text('Simpan'));
+      onPressed: () {
+        String nama = _namaController.text;
+        String nim = _nimController.text;
+        int tahun = int.parse(_tahunController.text);
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => TampilData(nama: nama, nim: nim, tahun: tahun),
+        ));
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 23, 178, 189), 
+        foregroundColor: Colors.white,    
+      ),
+      child: const Text('Simpan'),
+    );
   }
 }
